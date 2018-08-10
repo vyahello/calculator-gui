8fc38b79f9eb35ea011299cb11a9b9f79d958cdc
Volodymyr Yahello <vyahello@gmail.com>
2018-08-10 11:54:08 +0300

    Add screen type hints

--- lib/calculator/screen.py
+++ lib/calculator/screen.py
@@ -1,20 +1,21 @@
 from abc import ABC, abstractmethod
 from tkinter import Entry, StringVar
 from lib.calculator.types import TypeDisplay
+from lib.calculator.frame import Frame
 
 
 class Screen(ABC):
     """Abstract interface for a screen."""
 
     @abstractmethod
-    def grid(self, columnspan: int = 4) -> StringVar:
+    def grid(self, column_span: int = 4) -> StringVar:
         pass
 
 
 class CalculatorScreen(Screen):
     """Calculator screen."""
 
-    def __init__(self, frame, enter: TypeDisplay) -> None:
+    def __init__(self, frame: Frame, enter: TypeDisplay) -> None:
         self._screen: Entry = Entry(frame.element,
                                     font=('arial', 20, 'bold'),
                                     textvariable=enter.var,
@@ -23,5 +24,5 @@ class CalculatorScreen(Screen):
                                     bg="sky blue",
                                     justify='right')
 
-    def grid(self, columnspan: int = 4) -> StringVar:
-        return self._screen.grid(columnspan=columnspan)
+    def grid(self, column_span: int = 4) -> StringVar:
+        return self._screen.grid(columnspan=column_span)
